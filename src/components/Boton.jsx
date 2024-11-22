16bc314e2bc7ee9696cfdb2fe190327716101f20
Lavken <lavkenpilar@gmail.com>
2024-11-22 14:47:11 -0300

    createBoton-detro-componente

---
+++ src/components/Boton.jsx
@@ -0,0 +1,9 @@
+import { Button } from "react-bootstrap";
+const Boton = () => {
+    return (
+        <>
+         <Button variant="primary">Boton dentro de un componente</Button>
+        </>
+    )
+}
+export default Boton
--- src/components/MyCard.jsx
+++ src/components/MyCard.jsx
@@ -1,11 +1,13 @@
 import { Card } from "react-bootstrap";
+import Boton from "./Boton";
 
 const myCard = () => {
     return (
         <> <Card style={{ width: '18rem' }}>
         <Card.Img variant="top" src='public\imgs\mar-plata.png'/>
         <Card.Body>
-          <Card.Title>Card Title</Card.Title>
+          <Card.Title>Lavken</Card.Title>
+          <Boton/>
         </Card.Body>
       </Card>
         </>
